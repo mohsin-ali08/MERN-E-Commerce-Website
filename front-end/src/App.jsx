1bdb4595eade2430ca04eac65b23c4100eb97821
mohsin-ali08 <mohsinalisurhio08@gmail.com>
2025-09-10 20:36:47 +0500

    Completed landing page.

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import UserLayout from "./components/Layout/UserLayout";
 import Home from "./pages/Home";
+import { Toaster } from "sonner"; 
 
 function App() {
   return (
     <BrowserRouter> 
+    <Toaster position="top-right" reverseOrder={false} />
       <Routes>
         <Route path="/" element={<UserLayout />}>
           <Route index element={<Home />} />  
